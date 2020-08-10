1504d1b19c76993c0b02fa1f1edf924f891dba79
Guilherme Rodrigues <guilodron7@gmail.com>
2020-08-10 01:29:44 -0300

    Updates

---
+++ precache-manifest.467346b8ab52d24f69febcadf5e2c666.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b31cb5497ffa983c6e1b4979ceb58c2",
+    "url": "/survey-prototype/index.html"
+  },
+  {
+    "revision": "083c5d70c8281d6cc6db",
+    "url": "/survey-prototype/static/js/2.10218381.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/survey-prototype/static/js/2.10218381.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cec1637219952233a983",
+    "url": "/survey-prototype/static/js/main.5cfe3072.chunk.js"
+  },
+  {
+    "revision": "7c181891783d088bf756",
+    "url": "/survey-prototype/static/js/runtime-main.1e071d81.js"
+  }
+]);
